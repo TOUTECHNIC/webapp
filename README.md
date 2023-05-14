bea362bcd38c5159256385b4f29cedb8372e11b3
abdelaziz <75344731+TOUTECHNIC@users.noreply.github.com>
2023-05-14 02:13:40 +0100

    delete 3file

--- README.md
+++
@@ -1,4 +0,0 @@
-# webapp
-webapp first test API pfe lp2023
-you must install 3 extention
-
--- main.py
+++
@@ -1,8 +0,0 @@
-
-Msg=''' 
-        start test WEBAPP v1
-'''
-
-print(Msg)
-for i in range (40):
-    print(f"- {i}"*i)
